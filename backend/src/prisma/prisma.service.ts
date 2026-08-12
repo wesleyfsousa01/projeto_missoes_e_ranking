7cb20d14465b4e6a92f2254d1d7367f388e18901
wesleyfsousa01 <wesleyfsouza10@gmail.com>
2026-08-12 16:31:15 -0300

    feat(backend): implementa modulo e serviço do prisma.

---
+++ backend/src/prisma/prisma.service.ts
@@ -0,0 +1,21 @@
+import { Injectable, OnModuleDestroy, OnModuleInit } from '@nestjs/common';
+import { PrismaClient } from 'src/generated/prisma/client';
+import { PrismaPg } from "@prisma/adapter-pg";
+
+@Injectable()
+export class PrismaService extends PrismaClient implements OnModuleInit, OnModuleDestroy {
+    constructor() {
+        const adapter = new PrismaPg({
+            connectionString: process.env.DATABASE_URL as string,
+        });
+        super({ adapter });
+    }
+
+    async onModuleInit() {
+        await this.$connect();
+    }
+
+    async onModuleDestroy() {
+        await this.$disconnect();
+    }
+}
